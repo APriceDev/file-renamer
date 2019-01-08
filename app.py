e97200cf6b7f1e940f0cc83b9510aec2a8b40817
apricedev <alanprice.dev1@gmail.com>
2019-01-08 17:26:29 -0500

    added file name formatting

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import os
 data = os.listdir(os.getcwd() + r'\data')
 
 for f in data:
-  print(f)
+  file_name, file_ext = os.path.splitext(f)
+  file_prefix, file_body = file_name.split('-')
+  new_file_name = f'{file_body}{file_ext}'
+  print(new_file_name)
 
 
